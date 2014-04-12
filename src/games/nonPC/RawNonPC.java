ac1d8b6c05be46446a2f22834f98bb848ea40f3d
Markus (IIMOG) Ankenbrand <markus-ankenbrand@gmx.de>
2014-04-12 12:19:28 +0200

    Delayed background loading bug for NonPC games fixed

--- src/games/nonPC/RawNonPC.java
+++ src/games/nonPC/RawNonPC.java
@@ -5,17 +5,9 @@ import player.Player;
 
 public class RawNonPC extends StandardNonPC {
 	private static final long serialVersionUID = 1L;
-
-	private String background;
 	
 	public RawNonPC(Player[] player, int numOfRounds, Modus modus, String background, int globalGameID) {
 		super(player, numOfRounds, modus, background, globalGameID);
-		this.background = background;
-	}
-	
-	@Override
-	public void nowVisible(){
-		instance.changeBackground(background);
 	}
 
 }
