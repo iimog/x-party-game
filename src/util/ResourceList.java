d798beff781ae28738cb5933f16c0ad72402e9a3
Markus (IIMOG) Ankenbrand <markus-ankenbrand@gmx.de>
2014-04-16 07:10:56 +0200

    Method for Classpath listing implemented. License information included.

---
+++ src/util/ResourceList.java
@@ -0,0 +1,88 @@
+package util;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.util.jar.*;
+import java.security.*;
+
+public class ResourceList {
+	private static void collectURL(ResourceURLFilter f, Set<URL> s, URL u) {
+		if (f == null || f.accept(u)) {
+			s.add(u);
+		}
+	}
+
+	private static void iterateFileSystem(File r, ResourceURLFilter f,
+			Set<URL> s) throws MalformedURLException, IOException {
+		File[] files = r.listFiles();
+		for (File file : files) {
+			if (file.isDirectory()) {
+				iterateFileSystem(file, f, s);
+			} else if (file.isFile()) {
+				collectURL(f, s, file.toURI().toURL());
+			}
+		}
+	}
+
+	private static void iterateJarFile(File file, ResourceURLFilter f,
+			Set<URL> s) throws MalformedURLException, IOException {
+		JarFile jFile = new JarFile(file);
+		for (Enumeration<JarEntry> je = jFile.entries(); je.hasMoreElements();) {
+			JarEntry j = je.nextElement();
+			if (!j.isDirectory()) {
+				collectURL(f, s,
+						new URL("jar", "", file.toURI() + "!/" + j.getName()));
+			}
+		}
+		jFile.close();
+	}
+
+	private static void iterateEntry(File p, ResourceURLFilter f, Set<URL> s)
+			throws MalformedURLException, IOException {
+		if (p.isDirectory()) {
+			iterateFileSystem(p, f, s);
+		} else if (p.isFile() && p.getName().toLowerCase().endsWith(".jar")) {
+			iterateJarFile(p, f, s);
+		}
+	}
+
+	public static Set<URL> getResourceURLs() throws IOException,
+			URISyntaxException {
+		return getResourceURLs((ResourceURLFilter) null);
+	}
+
+	public static Set<URL> getResourceURLs(Class<?> rootClass)
+			throws IOException, URISyntaxException {
+		return getResourceURLs(rootClass, (ResourceURLFilter) null);
+	}
+
+	public static Set<URL> getResourceURLs(ResourceURLFilter filter)
+			throws IOException, URISyntaxException {
+		Set<URL> collectedURLs = new HashSet<>();
+		URLClassLoader ucl = (URLClassLoader) ClassLoader
+				.getSystemClassLoader();
+		for (URL url : ucl.getURLs()) {
+			iterateEntry(new File(url.toURI()), filter, collectedURLs);
+		}
+		return collectedURLs;
+	}
+
+	public static Set<URL> getResourceURLs(Class<?> rootClass,
+			ResourceURLFilter filter) throws IOException, URISyntaxException {
+		Set<URL> collectedURLs = new HashSet<>();
+		CodeSource src = rootClass.getProtectionDomain().getCodeSource();
+		iterateEntry(new File(src.getLocation().toURI()), filter, collectedURLs);
+		return collectedURLs;
+	}
+
+	public static void main(String args[]) {
+		try {
+			for (URL u : ResourceList.getResourceURLs()) {
+				System.out.println(u);
+			}
+		} catch (IOException | URISyntaxException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/util/ResourceURLFilter.java
@@ -0,0 +1,7 @@
+package util;
+
+import java.net.URL;
+
+public interface ResourceURLFilter {
+	public boolean accept(URL resourceUrl);
+}
