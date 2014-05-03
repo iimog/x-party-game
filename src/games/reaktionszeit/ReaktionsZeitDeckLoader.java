f07e890ecbcaa62c60409e4b2de59afaf28a7535
Markus (IIMOG) Ankenbrand <markus-ankenbrand@gmx.de>
2014-05-03 11:09:46 +0200

    fixed problem with Reaktionszeit (resources now loaded as url)

--- src/games/reaktionszeit/ReaktionsZeitDeckLoader.java
+++ src/games/reaktionszeit/ReaktionsZeitDeckLoader.java
@@ -5,24 +5,41 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import start.X;
+import util.ResourceList;
+import util.ResourceURLFilter;
 
 public class ReaktionsZeitDeckLoader {
 	// TODO systemFolder
-	private static File systemFolder = new File("/games/pc/reaktionszeit/");
+	private static String systemRoot = "/conf/pc/reaktionszeit/";
 	private static File userFolder = new File(X.getDataDir() + "games/pc/reaktionszeit/");
 
 	public static List<ReaktionsZeitDeck> loadReaktionsZeitDecks() {
 		List<ReaktionsZeitDeck> decks = new ArrayList<ReaktionsZeitDeck>();
-		String[] systemFiles = systemFolder.list();
-		for (String file : systemFiles) {
-			if (file.endsWith(".deck")) {
-				decks.add(fileToDeck(systemFolder+"/"+file, true));
+		try {
+			Set<URL> systemURLs = ResourceList
+					.getResourceURLs(new ResourceURLFilter() {
+						public @Override
+						boolean accept(URL u) {
+							String s = u.getFile();
+							return s.endsWith(".deck")
+									&& s.contains(systemRoot);
+						}
+					});
+			for (URL url : systemURLs) {
+				decks.add(urlToDeck(url));
 			}
-		}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}		
+		
 		if(userFolder.exists()){
 			String[] userFiles = userFolder.list();
 			for (String file : userFiles) {
@@ -53,6 +70,32 @@ public class ReaktionsZeitDeckLoader {
 			newDeck.relativeToAbsolute();
 		} catch (FileNotFoundException e) {
 			System.out.println("Datei: " + file
+					+ " nicht gefunden");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return newDeck;
+	}
+	
+	private static ReaktionsZeitDeck urlToDeck(URL url) {
+		ReaktionsZeitDeck newDeck = null;
+		try {
+			InputStream is = url.openStream();
+			BufferedReader br = new BufferedReader(new InputStreamReader(is));
+			newDeck = new ReaktionsZeitDeck(true);
+			String deckName = br.readLine();
+			newDeck.setDeckName(deckName);
+			String deckType = br.readLine();
+			newDeck.setDeckType(deckType);
+			while (br.ready()) {
+				String element = br.readLine();
+				newDeck.addElement(element);
+			}
+			br.close();
+			is.close();
+			newDeck.relativeToAbsolute();
+		} catch (FileNotFoundException e) {
+			System.out.println("URL: " + url
 					+ " nicht gefunden");
 		} catch (IOException e) {
 			e.printStackTrace();
