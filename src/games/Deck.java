533da2b25b49221b2bdb3c4cb83611c773964c7e
Markus (IIMOG) Ankenbrand <markus-ankenbrand@gmx.de>
2014-06-07 09:52:32 +0200

    Generalized Deck and DeckLoader classes. Can be used by every deck based game with minimal effort. Solving ticket #33

---
+++ src/games/Deck.java
@@ -0,0 +1,57 @@
+package games;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Deck {
+	private boolean system;	
+	private String deckName;
+	private String deckType;
+	private List<String> elements;
+	
+	public Deck(boolean system){
+		this.system = system;
+		elements = new ArrayList<String>();
+	}
+	
+	public Deck(Deck deck){
+		this.system = deck.isSystem();
+		this.deckName = deck.getDeckName();
+		this.deckType = deck.getDeckType();
+		this.elements = deck.getElements();
+	}
+
+	public void setDeckName(String deckName) {
+		this.deckName = deckName;
+	}
+	
+	public String getDeckName(){
+		return deckName;
+	}
+
+	public void addElement(String element) {
+		elements.add(element);
+	}
+
+	public void setDeckType(String deckType) {
+		this.deckType = deckType;
+	}
+
+	public String getDeckType() {
+		return deckType;
+	}
+
+	public List<String> getElements() {
+		return elements;
+	}
+	
+	public boolean isSystem(){
+		return system;
+	}
+	
+	public String toString(){
+		if(deckType == null)
+			return deckName;
+		return deckName + " (" + deckType + ")";
+	}
+}
---
+++ src/games/DeckLoader.java
@@ -0,0 +1,97 @@
+package games;
+
+import games.Deck;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import start.X;
+import util.ResourceList;
+import util.ResourceURLFilter;
+
+public class DeckLoader {
+	public static List<Deck> loadDecks(String game) {
+		List<Deck> decks = new ArrayList<Deck>();
+		final String systemRoot = "/conf/pc/"+game+"/";
+		File userFolder = new File(X.getDataDir() + "games/pc/"+game+"/");
+		try {
+			Set<URL> systemURLs = ResourceList
+					.getResourceURLs(new ResourceURLFilter() {
+						public @Override
+						boolean accept(URL u) {
+							String s = u.getFile();
+							return s.endsWith(".deck")
+									&& s.contains(systemRoot);
+						}
+					});
+			for (URL url : systemURLs) {
+				decks.add(urlToDeck(url));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}		
+		
+		if(userFolder.exists()){
+			String[] userFiles = userFolder.list();
+			for (String file : userFiles) {
+				if (file.endsWith(".deck")) {
+					decks.add(fileToDeck(userFolder+"/"+file, false));
+				}
+			}
+		}
+		return decks;
+	}
+
+	private static Deck fileToDeck(String file, boolean system) {
+		Deck newDeck = null;
+		try {
+			FileReader fr = new FileReader(file);
+			BufferedReader br = new BufferedReader(fr);
+			newDeck = bufferedReaderToDeck(br, system);
+			br.close();
+			fr.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("Datei: " + file
+					+ " nicht gefunden");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return newDeck;
+	}
+	
+	private static Deck urlToDeck(URL url) {
+		Deck newDeck = null;
+		try {
+			InputStream is = url.openStream();
+			BufferedReader br = new BufferedReader(new InputStreamReader(is));
+			newDeck = bufferedReaderToDeck(br, true);
+			br.close();
+			is.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return newDeck;
+	}
+	
+	private static Deck bufferedReaderToDeck(BufferedReader br, boolean system) throws IOException{
+		Deck newDeck = new Deck(system);
+		String deckName = br.readLine();
+		newDeck.setDeckName(deckName);
+		String deckType = br.readLine();
+		newDeck.setDeckType(deckType);
+		while (br.ready()) {
+			String element = br.readLine();
+			newDeck.addElement(element);
+		}
+		return newDeck;
+	}
+}
