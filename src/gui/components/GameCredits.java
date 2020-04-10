4c4a93cfc3e796a9cecbbed53808916078403853
Markus Ankenbrand <markus@ankenbrand.me>
2020-04-10 15:40:50 +0200

    Rewrite game credits indicators

--- src/gui/components/GameCredits.java
+++ src/gui/components/GameCredits.java
@@ -3,7 +3,10 @@ package gui.components;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
 import java.awt.GridLayout;
+import java.awt.Point;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -18,25 +21,44 @@ public class GameCredits extends JPanel {
 	 * 
 	 */
 	private static final long serialVersionUID = -7093227368252264203L;
+
 	public static void main(String[] args) throws InterruptedException { // Nur
 		// zum
 		// Testen
 		JFrame fenster = new JFrame("Testfenster");
+		JPanel mainPanel = new JPanel(new GridLayout(1, 3));
+		fenster.add(mainPanel);
 		GameCredits gc = new GameCredits();
-		fenster.add(gc);
+		GameCredits gc2 = new GameCredits(50, Color.BLUE);
+		GameCredits gc3 = new GameCredits(5000, Color.RED);
+		gc2.setLabelDistance(10);
+		gc3.setLabelDistance(500);
+		gc2.earnsCredit(23);
+		gc3.earnsCredit(120);
+		mainPanel.add(gc);
+		mainPanel.add(gc2);
+		mainPanel.add(gc3);
 		fenster.pack();
 		fenster.setVisible(true);
 		fenster.setLocationRelativeTo(null);
 		fenster.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Thread.sleep(1000);
 		gc.earnsCredit(1);
+		gc2.earnsCredit(1);
+		gc3.earnsCredit(1200);
 		Thread.sleep(1000);
 		gc.earnsCredit(1);
+		gc2.earnsCredit(13);
+		gc3.earnsCredit(250);
 		Thread.sleep(1000);
 		gc.earnsCredit(1);
+		gc2.earnsCredit(9);
+		gc3.earnsCredit(120);
+		gc.ko();
 	}
-	private GameCreditPart[] creds;
+
 	private int numOfRounds; // Anzahl der Gewinnsätzen
+
 	public int getNumOfRounds() {
 		return numOfRounds;
 	}
@@ -45,12 +67,14 @@ public class GameCredits extends JPanel {
 		this.numOfRounds = numOfRounds;
 		init();
 	}
+
 	private Color standardCol; // Standardhintergrundfarbe
 	private Color actionCol; // Standardfarbe für erreichte Punkte
 	private int credits = 0; // Schon gemachte Punkte
 	private boolean stechen;
 
 	private JLabel creditLabel;
+	private int labelDistance = 1;
 
 	public GameCredits() {
 		this(5, Color.ORANGE, Color.BLACK);
@@ -73,12 +97,7 @@ public class GameCredits extends JPanel {
 
 	public void earnsCredit(int credit) {
 		this.credits += credit;
-		for(GameCreditPart b: creds){
-			if(!b.isKO() && b.getNumber()<=credits){
-				b.activate();
-			}
-		}
-		creditLabel.setText(credits+"");
+		creditLabel.setText(credits + "");
 		repaint();
 	}
 
@@ -89,70 +108,75 @@ public class GameCredits extends JPanel {
 	private void init() {
 		removeAll();
 		setOpaque(false);
-		setLayout(new GridLayout(numOfRounds+1, 1));
-		creds = new GameCreditPart[numOfRounds];
-		creditLabel = new JLabel(credits+"");
+		setLayout(new BorderLayout());
+		creditLabel = new JLabel(credits + "");
 		creditLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		creditLabel.setOpaque(true);
 		creditLabel.setBackground(Color.darkGray);
 		creditLabel.setForeground(actionCol);
 		creditLabel.setFont(Game.STANDARD_FONT.deriveFont((float) 32.0));
-		add(creditLabel);
-		for (int i = (numOfRounds - 1); i >= 0; i--) {
-			creds[i] = new GameCreditPart(i + 1);
-			if(i < credits){
-				creds[i].activate();
-			}
-			add(creds[i]);
-		}
+		add(creditLabel, BorderLayout.NORTH);
+		GameCreditPanel credsPanel = new GameCreditPanel();
+		credsPanel.setPreferredSize(new Dimension(40, 4000));
+		// credsPanel.setLayout(new GridLayout(numOfRounds, 1));
+		add(credsPanel, BorderLayout.CENTER);
 	}
-	
-	public class GameCreditPart extends JPanel {
+
+	public class GameCreditPanel extends JPanel {
 		private static final long serialVersionUID = 1L;
-		private JLabel label;
-		private int number;
-		private boolean ko = false;
-		
-		GameCreditPart(int number){
-			super();
-			this.number = number;
-			label = new JLabel(""+number);
-			label.setHorizontalAlignment(SwingConstants.CENTER);
-			setLayout(new BorderLayout());
-			add(this.label, BorderLayout.CENTER);
-			setOpaque(true);
-			setBackground(standardCol);
-			setPreferredSize(new Dimension(40, 40));
-			
-			label.setForeground(actionCol);
+
+		public GameCreditPanel() {
+
 		}
-		
-		public void activate() {
-			setBackground(actionCol);
-			label.setForeground(standardCol);
+
+		@Override
+		protected void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			double fac = (double) credits / (double) numOfRounds;
+			int height = (int) Math.round(fac * this.getHeight());
+			g.setColor(standardCol);
+			g.fillRect(0, 0, this.getWidth(), this.getHeight());
+			g.setColor(actionCol);
+			g.fillRect(0, this.getHeight() - height, this.getWidth(), this.getHeight());
+			double oneUnit = (double) this.getHeight() / (double) numOfRounds;
+			for (int i = 1; i <= numOfRounds; i++) {
+				if (i % labelDistance == 0) {
+					int lineHeight = this.getHeight() - (int) Math.round(oneUnit * (i));
+					g.setColor(Color.DARK_GRAY);
+					g.drawLine(0, lineHeight, this.getWidth(), lineHeight);
+					g.setColor(i <= credits ? standardCol : actionCol);
+					int bumpDown = (int) Math.round(oneUnit * .5);
+					if (bumpDown < 8) {
+						bumpDown = 8;
+					}
+					String label = String.valueOf(i);
+					if (stechen) {
+						label = "K.O.";
+					}
+					drawCenteredString(g, label, new Point(this.getWidth() / 2, lineHeight + bumpDown));
+				}
+			}
 		}
-		
-		public void deactivate() {
-			setBackground(standardCol);
-			label.setForeground(actionCol);
-		}
-		
-		public int getNumber() {
-			return number;
-		}
-		
-		public void multiply(int factor) {
-			number *= factor;
-			label.setText(number+"");
-		}
-		
-		public void setKO() {
-			ko = true;
-			label.setText("K.O.");
-		}
-		
-		public boolean isKO() {
-			return ko;
+
+		/**
+		 * Draw a String centered in the middle of a Rectangle.
+		 *
+		 * @param g      The Graphics instance.
+		 * @param text   The String to draw.
+		 * @param center The Point to center the text around.
+		 * 
+		 *               inspired from https://stackoverflow.com/a/27740330
+		 */
+		public void drawCenteredString(Graphics g, String text, Point center) {
+			// Get the FontMetrics
+			FontMetrics metrics = g.getFontMetrics(g.getFont());
+			// Determine the X coordinate for the text
+			int x = center.x - (metrics.stringWidth(text) / 2);
+			// Determine the Y coordinate for the text (note we add the ascent, as in java
+			// 2d 0 is top of the screen)
+			int y = center.y - (metrics.getHeight() / 2) + metrics.getAscent();
+			// Draw the String
+			g.drawString(text, x, y);
 		}
 	}
 
@@ -162,13 +186,10 @@ public class GameCredits extends JPanel {
 
 	public void ko() {
 		setStechen(true);
-		creds[numOfRounds - 1].setKO();
 	}
 
-	public void multiply(int factor){
-		for(int i=0; i<creds.length; i++){
-			creds[i].multiply(factor);
-		}
+	public void setLabelDistance(int labelDistance) {
+		this.labelDistance = labelDistance;
 	}
 
 	public void setStechen(boolean stechen) {
